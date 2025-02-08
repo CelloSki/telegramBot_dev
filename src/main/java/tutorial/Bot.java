54377d5059689b6d246c932751c20bb46b9c46c1
Ahadjie Joshua S. <126101528+CelloSki@users.noreply.github.com>
2025-02-08 08:14:35 +0000

    Add files via upload

---
+++ src/main/java/tutorial/Bot.java
@@ -0,0 +1,41 @@
+package tutorial;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMediaGroup;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class Bot extends TelegramLongPollingBot {
+
+ @Override
+ public String getBotUsername() {
+ return "Cloud2LocalBot";
+ }
+
+ @Override
+ public String getBotToken() {
+ return "7831166511:AAGgCSkZA7f1REsJ8Uwn9B6JgxHDPtTETf8";
+ }
+
+ @Override
+ public void onUpdateReceived(Update update) {
+ Message message =update.getMessage();
+
+ if(message.hasText()) {
+     SendMessage sendMessage = new SendMessage();
+     Long chatId = message.getChatId();
+     sendMessage.setChatId(chatId.toString());
+     sendMessage.setText("Hello World");
+
+     try {
+         execute(sendMessage);
+     }catch (TelegramApiException e) {
+         e.printStackTrace();
+     }
+ }
+
+ }
+}
